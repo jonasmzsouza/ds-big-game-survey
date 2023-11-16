d1988aa136577350e37c2e57ee2ee0f0e12bc689
Jonas Souza <jonasmzsouza@gmail.com>
2023-11-16 17:38:34 -0300

    new(backend): project created

---
+++ backend/src/test/java/com/devsuperior/biggamesurvey/BiggamesurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.biggamesurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiggamesurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
